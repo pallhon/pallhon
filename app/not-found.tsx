47b1862b5183ce936835d0711a563375f676ce20
DerMatte <me@dermatte.de>
2023-03-14 21:25:27 +0100

    try new 404

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+      <main className="min-h-screen w-full overflow-x-hidden mix-blend-plus-lighter	invert flex flex-col justify-center items-center">
+            <h1 className="text-5xl md:text-7xl xl:text-9xl font-bold bg-blend-darken">
+              404 - Not Found
+            </h1>
+      </main>
+    );
+  }
